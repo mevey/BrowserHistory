3c5f2fe4b746734928eca5ea62e5de678b2d5601
mevey <muthoni90@gmail.com>
2018-04-20 10:55:50 -0700

    simple history size test

--- src/main/ci/BrowserHistory.java
+++ src/main/ci/BrowserHistory.java
@@ -1,7 +1,22 @@
 package ci;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class BrowserHistory {
+    List<String> history = new ArrayList<>();
+    Map<String, Integer>  historyMap = new HashMap<>();
     public BrowserHistory() {
 
+    }
+
+    public void visit(String page) {
+        history.add(page);
+    }
+
+    public int historySize() {
+        return history.size();
     }
 }
--- src/test/ci/BrowserHistoryTest.java
+++ src/test/ci/BrowserHistoryTest.java
@@ -2,10 +2,19 @@ package ci;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class BrowserHistoryTest {
 
+    BrowserHistory bh = new BrowserHistory();
+    public void setup() {
+        bh.visit("abc.com");
+    }
+
     @Test
-    public void emptytest() {
-        
+    public void afterVisitingOnePageTheSizeOfBrowserHistoryShouldIncreaseByOne() {
+        BrowserHistory bh = new BrowserHistory();
+        bh.visit("abc.com");
+        assertEquals(bh.historySize(), 1);
     }
 }
